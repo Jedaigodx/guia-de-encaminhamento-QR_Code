1b6215b06d4f486790c8fe3b97eb443e7018ccba
Jedaigodx <thallisson.h.tp@gmail.com>
2024-10-04 00:01:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 import qrcode
+import os
 
 link_sites ={
     "Tik_tok": "https://www.tiktok.com/foryou",
     "gmail":"https://mail.google.com/mail/u/0/#inbox",
     "Youtube":"https://www.youtube.com/"
 }
-
+ # criando um diretorio
+if not os.path.exists('qr_codes'):
+    os.makedirs('qr_codes')
 sites = link_sites
 
+#repeticao para gerar todos qr codes
 for meus_sites in sites:
     print(sites[meus_sites])
 
     imagem_qrcode = qrcode.make(sites[meus_sites])
-    imagem_qrcode.save(f"qrcode_{meus_sites}.png")
+    imagem_qrcode.save(f"qr_codes/qrcode_{meus_sites}.png")
